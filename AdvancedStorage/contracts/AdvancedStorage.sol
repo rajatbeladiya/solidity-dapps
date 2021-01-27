d5866f26cfeea83b804d53565ed8f489273794f1
Rajat Beladiya <rajatbeladiya7@gmail.com>
2021-01-27 09:49:14 +0530

    Advanced Storage

---
+++ AdvancedStorage/contracts/AdvancedStorage.sol
@@ -0,0 +1,22 @@
+pragma solidity ^0.5.0;
+
+contract AdvancedStorage {
+    uint[] public ids;
+    
+    function add(uint id) public {
+        ids.push(id);
+    }
+    
+    function get(uint position) view public returns(uint) {
+        return ids[position];
+    }
+    
+    function getAll() view public returns(uint[] memory _array) {
+        return ids;
+    }
+    
+    function length() view public returns (uint) {
+        return ids.length;
+    }
+    
+}
