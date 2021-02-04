b17a7c2460d52af064e75afcf4178f7fffb05c65
Rajat Beladiya <rajatbeladiya7@gmail.com>
2021-02-04 09:51:59 +0530

    Escrow Contract added

---
+++ escrow/test/Escrow.js
@@ -0,0 +1,17 @@
+const Escrow = artifacts.require("Escrow");
+
+const assertError = async (promise, error) => {
+  try {
+    await promise;
+  } catch (e) {
+    assert(e.message.includes(error));
+  }
+  assert(false);
+}
+
+contract('Escrow', () => {
+  let escrow = null;
+  before(async () => {
+    escrow = await Escrow.deployed();
+  });
+});
